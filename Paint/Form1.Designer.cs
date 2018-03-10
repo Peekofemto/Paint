44f78b775a60e8d353ae0a6724559d06320a6851
Peeko <omarock27@gmail.com>
2018-03-09 19:40:15 -0700

    Basic layout design
    Design of the main form, doesnt have any functionality yet

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Paint
+{
+    partial class formPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFiguras = new System.Windows.Forms.Button();
+            this.btnLinea = new System.Windows.Forms.Button();
+            this.btnRelleno = new System.Windows.Forms.Button();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.btnRectangulo = new System.Windows.Forms.Button();
+            this.btnTriangulo = new System.Windows.Forms.Button();
+            this.btnManoAlzada = new System.Windows.Forms.Button();
+            this.btnPoligono = new System.Windows.Forms.Button();
+            this.btnTexto = new System.Windows.Forms.Button();
+            this.btnParametros = new System.Windows.Forms.Button();
+            this.gbHerramientas = new System.Windows.Forms.GroupBox();
+            this.gbAcciones = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gbParametros = new System.Windows.Forms.GroupBox();
+            this.gbHerramientas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(102, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1160, 709);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnFiguras
+            // 
+            this.btnFiguras.Location = new System.Drawing.Point(0, 11);
+            this.btnFiguras.Name = "btnFiguras";
+            this.btnFiguras.Size = new System.Drawing.Size(102, 54);
+            this.btnFiguras.TabIndex = 1;
+            this.btnFiguras.Text = "Figuras";
+            this.btnFiguras.UseVisualStyleBackColor = true;
+            // 
+            // btnLinea
+            // 
+            this.btnLinea.Location = new System.Drawing.Point(0, 62);
+            this.btnLinea.Name = "btnLinea";
+            this.btnLinea.Size = new System.Drawing.Size(102, 54);
+            this.btnLinea.TabIndex = 2;
+            this.btnLinea.Text = "Linea";
+            this.btnLinea.UseVisualStyleBackColor = true;
+            // 
+            // btnRelleno
+            // 
+            this.btnRelleno.Location = new System.Drawing.Point(0, 111);
+            this.btnRelleno.Name = "btnRelleno";
+            this.btnRelleno.Size = new System.Drawing.Size(102, 54);
+            this.btnRelleno.TabIndex = 3;
+            this.btnRelleno.Text = "Relleno";
+            this.btnRelleno.UseVisualStyleBackColor = true;
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.Location = new System.Drawing.Point(0, 162);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(102, 54);
+            this.btnCirculo.TabIndex = 4;
+            this.btnCirculo.Text = "Circulo";
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            // 
+            // btnRectangulo
+            // 
+            this.btnRectangulo.Location = new System.Drawing.Point(0, 213);
+            this.btnRectangulo.Name = "btnRectangulo";
+            this.btnRectangulo.Size = new System.Drawing.Size(102, 54);
+            this.btnRectangulo.TabIndex = 5;
+            this.btnRectangulo.Text = "Rectangulo";
+            this.btnRectangulo.UseVisualStyleBackColor = true;
+            // 
+            // btnTriangulo
+            // 
+            this.btnTriangulo.Location = new System.Drawing.Point(0, 264);
+            this.btnTriangulo.Name = "btnTriangulo";
+            this.btnTriangulo.Size = new System.Drawing.Size(102, 54);
+            this.btnTriangulo.TabIndex = 6;
+            this.btnTriangulo.Text = "Triangulo";
+            this.btnTriangulo.UseVisualStyleBackColor = true;
+            // 
+            // btnManoAlzada
+            // 
+            this.btnManoAlzada.Location = new System.Drawing.Point(0, 315);
+            this.btnManoAlzada.Name = "btnManoAlzada";
+            this.btnManoAlzada.Size = new System.Drawing.Size(102, 54);
+            this.btnManoAlzada.TabIndex = 7;
+            this.btnManoAlzada.Text = "Mano Alzada";
+            this.btnManoAlzada.UseVisualStyleBackColor = true;
+            // 
+            // btnPoligono
+            // 
+            this.btnPoligono.Location = new System.Drawing.Point(0, 366);
+            this.btnPoligono.Name = "btnPoligono";
+            this.btnPoligono.Size = new System.Drawing.Size(102, 54);
+            this.btnPoligono.TabIndex = 8;
+            this.btnPoligono.Text = "Polígono";
+            this.btnPoligono.UseVisualStyleBackColor = true;
+            // 
+            // btnTexto
+            // 
+            this.btnTexto.Location = new System.Drawing.Point(0, 416);
+            this.btnTexto.Name = "btnTexto";
+            this.btnTexto.Size = new System.Drawing.Size(102, 54);
+            this.btnTexto.TabIndex = 9;
+            this.btnTexto.Text = "Texto";
+            this.btnTexto.UseVisualStyleBackColor = true;
+            // 
+            // btnParametros
+            // 
+            this.btnParametros.Location = new System.Drawing.Point(0, 466);
+            this.btnParametros.Name = "btnParametros";
+            this.btnParametros.Size = new System.Drawing.Size(102, 54);
+            this.btnParametros.TabIndex = 10;
+            this.btnParametros.Text = "Parametros";
+            this.btnParametros.UseVisualStyleBackColor = true;
+            // 
+            // gbHerramientas
+            // 
+            this.gbHerramientas.Controls.Add(this.groupBox2);
+            this.gbHerramientas.Controls.Add(this.btnParametros);
+            this.gbHerramientas.Controls.Add(this.btnRectangulo);
+            this.gbHerramientas.Controls.Add(this.btnLinea);
+            this.gbHerramientas.Controls.Add(this.btnRelleno);
+            this.gbHerramientas.Controls.Add(this.btnManoAlzada);
+            this.gbHerramientas.Controls.Add(this.btnTexto);
+            this.gbHerramientas.Controls.Add(this.btnPoligono);
+            this.gbHerramientas.Controls.Add(this.btnTriangulo);
+            this.gbHerramientas.Controls.Add(this.btnCirculo);
+            this.gbHerramientas.Controls.Add(this.btnFiguras);
+            this.gbHerramientas.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gbHerramientas.Location = new System.Drawing.Point(0, 0);
+            this.gbHerramientas.Name = "gbHerramientas";
+            this.gbHerramientas.Size = new System.Drawing.Size(102, 709);
+            this.gbHerramientas.TabIndex = 0;
+            this.gbHerramientas.TabStop = false;
+            this.gbHerramientas.Text = "Herramientas";
+            // 
+            // gbAcciones
+            // 
+            this.gbAcciones.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gbAcciones.Location = new System.Drawing.Point(102, 595);
+            this.gbAcciones.Name = "gbAcciones";
+            this.gbAcciones.Size = new System.Drawing.Size(1160, 114);
+            this.gbAcciones.TabIndex = 1;
+            this.gbAcciones.TabStop = false;
+            this.gbAcciones.Text = "Acciones";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(0, 526);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(102, 184);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // gbParametros
+            // 
+            this.gbParametros.Location = new System.Drawing.Point(0, 518);
+            this.gbParametros.Name = "gbParametros";
+            this.gbParametros.Size = new System.Drawing.Size(102, 193);
+            this.gbParametros.TabIndex = 2;
+            this.gbParametros.TabStop = false;
+            this.gbParametros.Text = "Misc";
+            // 
+            // formPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 709);
+            this.Controls.Add(this.gbParametros);
+            this.Controls.Add(this.gbAcciones);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gbHerramientas);
+            this.Name = "formPrincipal";
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.formPrincipal_Load);
+            this.gbHerramientas.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFiguras;
+        private System.Windows.Forms.Button btnLinea;
+        private System.Windows.Forms.Button btnRelleno;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Button btnRectangulo;
+        private System.Windows.Forms.Button btnTriangulo;
+        private System.Windows.Forms.Button btnManoAlzada;
+        private System.Windows.Forms.Button btnPoligono;
+        private System.Windows.Forms.Button btnTexto;
+        private System.Windows.Forms.Button btnParametros;
+        private System.Windows.Forms.GroupBox gbHerramientas;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox gbAcciones;
+        private System.Windows.Forms.GroupBox gbParametros;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class formPrincipal : Form
+    {
+        public formPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void formPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
